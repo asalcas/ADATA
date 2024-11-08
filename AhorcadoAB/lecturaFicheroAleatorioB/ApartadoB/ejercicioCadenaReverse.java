833bf2ec385ce79c7bfa4d5e87d115fa8c19009b
BaiHuu <alvaro.salvador@iesnervion.es>
2024-11-08 10:17:25 +0100

    ADATA falta el c

---
+++ AhorcadoAB/lecturaFicheroAleatorioB/ApartadoB/ejercicioCadenaReverse.java
@@ -0,0 +1,44 @@
+package ApartadoB;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class ejercicioCadenaReverse {
+
+    public static void main (String... args){
+
+        String rutaArchivoInput = "C:\\Users\\alvaro.salvador\\Desktop\\GitHub\\ADATA\\AhorcadoAB\\lecturaFicheroAleatorioB\\ApartadoB\\letras.txt";
+        String rutaArchivoOutput = "C:\\Users\\alvaro.salvador\\Desktop\\GitHub\\ADATA\\AhorcadoAB\\lecturaFicheroAleatorioB\\ApartadoB\\letrasReverse.txt";
+        
+        String letra;
+
+        try (RandomAccessFile lector = new RandomAccessFile(rutaArchivoInput, "r");
+            RandomAccessFile escritor = new RandomAccessFile(rutaArchivoOutput, "rw")){
+
+                for (long i = lector.length()-1; i >= 0; i-=3){
+                    
+                    lector.seek(i);
+                    letra= lector.readLine();
+                    escritor.writeBytes(letra);
+                    if (i > 0){
+                        escritor.writeBytes("\n");
+                    }
+
+                    /*if (letra == "\n"){
+                        i--;
+                    }
+                    */
+                }
+
+
+            
+                
+
+        }catch(IOException error) {
+            System.out.println("Ha ocurrido un error inesperado: " + error);
+
+        }
+
+    }
+}
+
